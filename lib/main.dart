63852ee1a25f2c3ca62ddf388da5a9c4c7d39a3d
Ressurectionx <Rajkumarpujari111@gmail.com>
2022-01-23 11:40:51 +0530

    Scroll Controller added

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,27 @@ import 'package:flutter/services.dart';
 import 'package:music/music.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Color(0xff4e03a7),systemNavigationBarColor: Color(0xff0a35ad ) ));
+
+
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        statusBarColor: const Color(0xff302b63),
+        systemNavigationBarColor: Color(0xff08061d) ));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+
         backgroundColor: Colors.purple.shade300,
       ),
-      home: Music(),
+      home: const Music(),
     );
   }
 }
